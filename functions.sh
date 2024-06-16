58de916d5c7365b2823c40c6f75d3e2ad6c67c60
MrCee <10969457+MrCee@users.noreply.github.com>
2024-06-16 18:05:28 +0000

    code cleanup

--- functions.sh
+++ functions.sh
@@ -11,29 +11,22 @@ fi
 #   $1 - Sed expression
 #   $2 - Input file
 func_sed() {
-    # Define a temporary file for storing the modified content
     local tmp_file=$(mktemp)
-
-    # Perform the sed operation and capture the output and exit code
     sed_output=$(sed -E "$1" "$2" > "$tmp_file" 2>&1)
     sed_exit_code=$?
 
     if [ $sed_exit_code -eq 0 ]; then
-        # Check if any changes were made
         if cmp -s "$2" "$tmp_file"; then
             echo "No changes needed in '$2'."
         else
-            # Replace the original file with the modified content
             mv "$tmp_file" "$2"
             echo "Fix applied in '$2' :: '$1'"
         fi
     else
-        # Print an error message if the sed operation failed
         echo "Error: Sed operation failed - $sed_output"
         return $sed_exit_code
     fi
-
-    # Clean up the temporary file if it still exists
-    [ -f "$tmp_file" ] && rm "$tmp_file"
+    
+    [[ -f "$tmp_file" ]] && rm "$tmp_file"
 }
 
--- zsh_config.sh
+++ zsh_config.sh
@@ -4,23 +4,12 @@
 install_bat_theme() {
     local theme_name="tokyonight_night"
 
-    # Check if bat package installation is required
     if [[ $(echo "$CONFIG_JSON" | jq -r '.packages.bat.install') == true ]]; then
         echo "Installing bat theme: $theme_name"
-        
-        # Create themes directory if it doesn't exist
         mkdir -p "$(bat --config-dir)/themes"
-        
-        # Change to themes directory
         cd "$(bat --config-dir)/themes" || exit
-        
-        # Download the theme
         curl -O "https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/${theme_name}.tmTheme"
-        
-        # Rebuild bat cache
         bat cache --build
-        
-        # Verify theme installation and check if theme is already in the config file
         if bat --list-themes | grep -q "$theme_name" && ! grep -q "\--theme=\"$theme_name\"" "$(bat --config-dir)/config"; then
             echo "bat theme: $theme_name installed successfully"
             echo "--theme=\"$theme_name\"" >> "$(bat --config-dir)/config"
@@ -37,12 +26,10 @@ CONFIG_JSON="$1"
 
 [[ -z "$CONFIG_JSON" ]] && CONFIG_JSON=$(cat "./config.json")
 
-# Check if the JSON has data
 if [ -z "$CONFIG_JSON" ]; then
     echo "Error: JSON configuration file not found"
     exit 1
 fi
 
-# Install bat theme if needed
 install_bat_theme
 
